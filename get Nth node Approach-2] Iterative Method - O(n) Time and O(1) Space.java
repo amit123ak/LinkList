10fb40eb32689e7af2a3827c518ac2a32cf23a21
amit123ak <41484488+amit123ak@users.noreply.github.com>
2026-02-12 21:27:28 +0530

    Implement iterative method to get Nth node data

--- get Nth node Approach-2] Iterative Method - O(n) Time and O(1) Space.java
+++ get Nth node Approach-2] Iterative Method - O(n) Time and O(1) Space.java
@@ -1,4 +1,38 @@
-public static int  givenNodeDataAtGivenPos(Node head, int index)
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class Main {
+    
+ static class Node {
+     
+     int data;
+     Node next;
+     Node(int data)
+     {
+         this.data=data;
+         this.next=null;
+     }
+ }
+ 
+ 
+  public static void printNode(Node head)
+  {
+      
+       Node curr=head;
+       while(curr!=null)
+       {
+            System.out.print(curr.data+"->");
+             
+           curr=curr.next;
+       }
+       if(curr==null)
+       {
+           System.out.println("null");
+       }
+  }
+  
+  public static int  givenNodeDataAtGivenPos(Node head, int index)
   {
       
       if(head==null)
@@ -21,3 +55,28 @@ public static int  givenNodeDataAtGivenPos(Node head, int index)
     
   }
  
+ 
+ 
+    public static void main(String[] args) {
+         Node head =new Node(10);
+         head.next=new Node(20);
+         head.next.next=new Node(30);
+         head.next.next.next=new Node(40);
+         printNode(head);
+         
+         System.out.println("index of 3");
+         int c= givenNodeDataAtGivenPos(head,3);
+            System.out.println(c);
+
+         
+        
+        
+
+
+     
+       
+         
+         
+   
+    }
+}
